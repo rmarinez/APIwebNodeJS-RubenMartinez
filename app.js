3e73601f8a2277a2c3390d844ea9777c2c15f3ec
Your Name <you@example.com>
2022-05-13 15:39:11 +0200

    sa

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express')
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/products", (req,res) => {
-   const products = [
-     {
-       id: 1,
-       name: "hammer",
-     },
-     {
-       id: 2,
-       name: "screwdriver",
-     },
-     ,
-     {
-       id: 3,
-       name: "wrench",
-     },
-   ];
+function isAuthorized(req,res, next) {
+  const auth = req.headers.authorization;
+  if (auth === 'secretpasswordRuben') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Not permitted');
+  }
+}
 
-  res.json(products);
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/users', isAuthorized, (req,res) => {
+  res.json([{
+    id: 1,
+    name: 'User Userson'
+  }])
+ })
+
+app.get('/products', (req, res) => {
+  res.json([{
+    id: 1,
+    name: 'The Bluest Eye'
+  }])
 })
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
